d0747395725cb4bf08dd27eea257b759389d5772
arivusurya <arivusurya.3@gmail.com>
2023-04-30 22:44:37 +0530

    Object and class

---
+++ Desktop/dotnet-developement/dotnet-remap-assinment/oops-concepts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using oops_concepts;
+
+
+namespace Program {
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        // objects
+           Vechile bmw = new Vechile("BMW");
+        }
+    }
+}
---
+++ Desktop/dotnet-developement/dotnet-remap-assinment/oops-concepts/newclass.cs
@@ -0,0 +1,13 @@
+using System;
+namespace oops_concepts
+{
+    // class that blueprint of an real world entities 
+    // it also have the properties{variable for that perticular class} and methods to manipulate data;
+    public class Vechile
+    {   
+
+       public Vechile(string brand){
+            Console.WriteLine($"Welcome to  {brand}");
+       }
+    }
+}
